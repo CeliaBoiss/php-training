b9de5db51b228d704fae6608a4dd4f671819e7c0
CeliaBoiss <70576120+CeliaBoiss@users.noreply.github.com>
2020-09-27 16:27:21 +0200

    Add files via upload

---
+++ indiana1.php
@@ -0,0 +1,12 @@
+<?php
+    $movieName = "Indiana Jones and the Last Crusade";
+    $seeMovie = true;
+    $movieYear = 1989;
+    $movieRate = 8.2;
+
+    echo $movieName ."<br />";
+    echo $seeMovie ."<br />";
+    echo $movieYear ."<br />";
+    echo $movieRate ."<br />";
+
+?>
